fd252c85891c22f1203c21cccf216a6cdb54de3b
Gabriel Arroyo <gabriel_eleazar.arroyo@daimlertruck.com>
2023-08-03 15:52:20 -0600

    test title

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, Gabo!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Innvie lock API", "message": "Welcome to FastAPI!"}
